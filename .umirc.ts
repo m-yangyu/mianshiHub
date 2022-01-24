bc73a68a0911e18f041f2f68bc951cdcd693c1d5
yck <yuchengkai@kuaishou.com>
2022-01-24 13:40:21 +0800

    chore: 工程配置

--- .umirc.ts
+++ .umirc.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from 'umi';
+import { VanillaExtractPlugin } from '@vanilla-extract/webpack-plugin';
 
 export default defineConfig({
   nodeModulesTransform: {
@@ -6,4 +7,15 @@ export default defineConfig({
   },
   routes: [{ path: '/', component: '@/pages/index' }],
   fastRefresh: {},
+  webpack5: {},
+  extraPostCSSPlugins: [
+    require('postcss-import'),
+    require('tailwindcss')({
+      config: './tailwind.config.js',
+    }),
+    require('postcss-nested'),
+  ],
+  chainWebpack: (memo) => {
+    memo.plugin('vanilla-extract').use(VanillaExtractPlugin);
+  },
 });
